dee1724541b6ed90bf756f98bb11009fbec02b42
Alafonye K. Emmanuel <ke.alafonye@yahoo.com>
2022-04-09 10:36:45 -0600

    working file upload

--- Backend/routes/uploadRouter.js
+++ Backend/routes/uploadRouter.js
@@ -2,13 +2,10 @@ const express = require("express");
 const multer = require("multer");
 const uploadRouter = express.Router();
 
-const upload = multer({storage: storage, limits: {fileSize: 1000000}, imageFileFilter}).single("parksImage"); //multer middleware
-    //Handling Routers to accepting posting Image.
-
     //Using DiskStorage Method(des:func, filename: func)
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, "uploads");
+        cb(null, "public/images");
     },
     filename: function(req, file, cb){
         cb(null, file.originalname);
@@ -23,6 +20,13 @@ const imageFileFilter = function(req, file, cb){
     cb(null, true);
 };
 
+const upload = multer({
+    storage: storage, 
+    limits: {fileSize: 1000000}, 
+    imageFileFilter})
+    .single("parksImage"); //multer middleware
+    //Handling Routers to accepting posting Image.
+
     //Router to handle upload
 uploadRouter.route("/")
 .all((req, res, next) => {
@@ -35,19 +39,9 @@ uploadRouter.route("/")
     res.end(`This request is not supported!`);
 })
 .post(upload, (req, res) => {
-    //Double check
-    upload(req, res, (err) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.json(req.file);
-        if(err) {
-            res.status = 400;
-            res.send(`Ensure you are submiting and image`);
-        }
-        else{
-            res.send(req.file);
-        }
-    });
 });
 
 module.exports = uploadRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
 const parkRouter = require("./routes/parkRouter");
 const uploadRouter = require("./routes/uploadRouter");
 
-
+app.use("/static", express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
     //use /mellow to create the routes
 app.use('/', parkRouter);
-app.use('/', uploadRouter);
+app.use('/upload', uploadRouter);
 
 
 app.listen(PORT, function(){
