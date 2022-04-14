ffc0820d66705dbe71c36b970c0a6825358f164c
Alafonye K. Emmanuel <ke.alafonye@yahoo.com>
2022-04-13 18:58:18 -0600

    React-router-dom for Navigations is w done

---
+++ Frontend/mellowgroup/src/Components/Recreation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recreation = () => {
+  return (
+    <div>Recreation</div>
+  )
+}
+
+export default Recreation;
--- Frontend/mellowgroup/src/index.js
+++ Frontend/mellowgroup/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router >
+  // <React.StrictMode>
+    <BrowserRouter >
       <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    </BrowserRouter>
+  // </React.StrictMode>
+  ,document.getElementById('root')
 );
 
 reportWebVitals();
