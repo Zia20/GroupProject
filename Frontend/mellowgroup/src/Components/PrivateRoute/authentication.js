7d38ce9409211b9ab3b65f45b6124848a9d7ace7
Alafonye K. Emmanuel <ke.alafonye@yahoo.com>
2022-04-21 08:30:45 -0600

    auth and private routes

---
+++ Frontend/mellowgroup/src/Components/PrivateRoute/authentication.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const authentication = () => {
+  return (
+    <div>
+     This is git issue testing
+        <Component />
+    </div>
+  )
+}
+
+export default authentication
