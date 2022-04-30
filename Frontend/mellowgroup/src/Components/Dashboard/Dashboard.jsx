2f56b99ff5ac85084aa258e25e395cb8265396a0
Alafonye K. Emmanuel <ke.alafonye@yahoo.com>
2022-04-29 19:09:16 -0600

    Users Login and Navigation

--- Frontend/mellowgroup/src/Components/Dashboard/Dashboard.jsx
+++ Frontend/mellowgroup/src/Components/Dashboard/Dashboard.jsx
@@ -1,129 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Row, Col } from 'react-bootstrap';
-// import { a } from "react-router-dom";
+// import jwt from "jsonwebtoken";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const [users, setUsers] = useState();
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   // if(token) {
+  //   //   const users = jwt.decode(token)
+  //   //   console.log(users)
+  //   // } else if(!users){
+  //   //   localStorage.removeItem("token");
+  //   //   navigate("/login")
+      
+  //   // }
+  // }, [])
 
 const sidenav =  {
     height: "100%",
-    width: "170px",
+    width: "220px",
     position: "fixed",
     zIndex: "0",
     top: "30",
-    // marginTop:"200px",
     left: "0",
-    // bottom: "30px",
     backgroundColor: "CadetBlue",
     overflowX: "hidden",
     paddingTop: "20px",
   }
 
-
   return (
-    <div>
-    <Container fluid='md'>
-      <Row>
-        <div style={sidenav}>
-          <div>
-            <a className="navbar-brand" href="#">
-              Mellow
-            </a>
+    <>
+      <div style={sidenav} >
+        <ul className="list-unstyled mx-2 text-success unline-none">
+          <h1><li><a href="#" className="f-50 text-decoration-none">Dashboard</a></li></h1>
+          <h1><li><a href="#" className="f-50 text-decoration-none">Recreation</a></li></h1>
+          <h1><li><a href="#" className="f-50 text-decoration-none">Complains</a></li></h1>
+          <h1><li><a href="#" className="f-50 text-decoration-none">Signups</a></li></h1>
+          <h1><li><a href="#" className="f-50 text-decoration-none">Ratings</a></li></h1>
+        </ul>
+      </div>
+      <div>
+        <div className="bg-info p-5 mb-1 text-white text-center">
+          <h4>Mellow</h4>
+          <p>Calgary Citizen Reporting</p>
+        </div>
+        <div className="row w-75 mx-auto text-align-center mt-2">
+          <div className="col-sm-3 bg-danger  p-4 text-white">
+            <h4>Park Users</h4>
+            <p>10K</p>
           </div>
-          <div>
-            <ul >
-              <li>
-                <a href="#">Dashboard</a>
-              </li>
-              <li>
-                <a href="#">Complains</a>
-              </li>
-              <li>
-                <a href="#">Signup</a>
-              </li>
-              <li>
-                <a href="#">Ratings</a>
-              </li>
-            </ul>
+          <div className="col-sm-3 bg-primary  p-4 text-white">
+            <h4>Cases Resolve</h4>
+            <p>1K</p>
+          </div>
+          <div className="col-sm-3 bg-warning  p-4 text-white">
+            <h4>Total Complains</h4>
+            <p>10 Million</p>
+          </div>
+          <div className="col-sm-3 bg-dark  p-4 text-white">
+            <h4>Citizen Safety</h4>
+            <p>30%</p>
           </div>
         </div>
-        
-        <div className="container fluid='md'">
-        {/* <div class="row justify-content-sm-center mb-5"> */}
-        <div className="row content mb-5" >
-          <div className="col-lg-12 px-1 mx-5 ">
-            <div className="well bg-info d-block p-5 mb-1 text-white text-center">
-              <h4>Mellow</h4>
-              <p>Some text..</p>
-            </div>
-            <div className="row content">
-              <div className="col-sm-3 bg-danger d-block p-4 text-white">
-                <div className="well">
-                  <h4>Users</h4>
-                  <p>1 Million</p>
-                </div>
-              </div>
-              <div className="col-sm-3 bg-primary d-block p-4 text-white">
-                <div className="well">
-                  <h4>Pages</h4>
-                  <p>100 Million</p>
-                </div>
-              </div>
-              <div className="col-sm-3 bg-warning d-block p-4 text-white">
-                <div className="well">
-                  <h4>Sessions</h4>
-                  <p>10 Million</p>
-                </div>
-              </div>
-              <div className="col-sm-3 bg-dark d-block p-4 text-white">
-                <div className="well">
-                  <h4>Bounce</h4>
-                  <p>30%</p>
-                </div>
-              </div>
-            </div>
-            <div className="row ">
-              <div className="col-sm-4 bg-secondary d-block p-4 text-white">
-                <div className="well">
-                  <p>Text</p>
-                  <p>Text</p>
-                  <p>Text</p>
-                </div>
-              </div>
-              <div className="col-sm-4 bg-info d-block p-4 text-white">
-                <div className="well">
-                  <p>Text</p>
-                  <p>Text</p>
-                  <p>Text</p>
-                </div>
-              </div>
-              <div className="col-sm-4 bg-danger d-block p-4 text-white">
-                <div className="well">
-                  <p>Text</p>
-                  <p>Text</p>
-                  <p>Text</p>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-8 bg-warning d-block p-4 text-white">
-                <div className="well">
-                  <p>Text</p>
-                </div>
-              </div>
-              <div className="col-sm-4 bg-danger d-block p-4 text-white">
-                <div className="well">
-                  <p>Text</p>
-                </div>
-              </div>
-            </div>
+        <div className="row w-75 mx-auto text-align-center mt-2">
+          <div className="col-sm-4 bg-secondary  p-4 text-white">
+            <p>Dog Area Searches</p>
+            <p>Dog Incident</p>
+            <p>Maintenance</p>
+          </div>
+          <div className="col-sm-4 bg-info  p-4 text-white">
+            <p>Hospital Report</p>
+            <p>911 Calls</p>
+            <p>Emergencey</p>
+          </div>
+          <div className="col-sm-4 bg-danger  p-4 text-white">
+            <p>Wildlife</p>
+            <p>Injuries</p>
+            <p>Signs</p>
+          </div>
+        </div>
+        <div className="row w-75 mx-auto text-align-center mt-2">
+          <div className="col-sm-6 bg-warning  p-4 text-white">
+            <h3>Complains Resolve</h3>
+            <p>30% Cases resolved</p>
+          </div>
+          <div className="col-sm-6 bg-primary p-4 text-white">
+            <p>Wildlife Interactions</p>
+          </div>
+        </div>
+        <div className="row w-75 mx-auto text-align-center mt-2">
+          <div className="col-sm-6 bg-dark  p-4 text-white">
+            <h3>Overal</h3>
+            <p>30% Cases resolved</p>
+          </div>
+          <div className="col-sm-6 bg-success p-4 text-white">
+            <p>Improvemnts</p>
           </div>
         </div>
       </div>
-      </Row>
-    </Container>
-    </div>
+    </>
   );
 };
 
--- Frontend/mellowgroup/src/Components/Headers/Navigation.jsx
+++ Frontend/mellowgroup/src/Components/Headers/Navigation.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightToBracket, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faRightToBracket, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 import { myBLink, myLink, NavbarStyle  } from "../Styles/Styles";
 
 function Navigation() {
+ 
+  const [user, setUsers ] = useState();
+  // const user = null;
+  // useEffect(() => {
+  //   const getUser = async() => {
+  //     try {
+  //       await fetch("/user", {
+  //         method: "GET",
+  //         headers: { "Content-Type": "application/json"},
+  //         credentials: "include",
+  //       })
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   }
+  //   getUser();
+  // }, []);
 
   return (
     <>
       <Navbar bg="success" expand="lg" className='py-3 mx-auto'>
-        <Container fluid>
+        
+        {user ? (
+          <Container fluid>
           <Navbar.Brand style={NavbarStyle}><Link to="/" style={myBLink}>Me<span className='text-danger'>llow</span></Link></Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
@@ -27,7 +46,33 @@ function Navigation() {
             </Nav>
             <Form className="d-flex">
               <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
-              <Link to="/search" style={myLink}><Button className='shadow-none className="mx-2"' variant="outline-dark">Search</Button></Link>
+              <Link to="/search" style={myLink}><Button className='shadow-none mx-2' variant="outline-dark">Search</Button></Link>
+            </Form>
+            <Form className="d-flex">
+            <Link to="/signup" style={myLink}><Button className='shadow-none mx-2' variant="outline-dark"><FontAwesomeIcon icon={faUserPlus}/>{user.name}</Button></Link>
+            </Form>
+            <Form className="d-flex">
+            <Link to="/signin" style={myLink}><Button className='shadow-none mx-2' variant="outline-dark"><FontAwesomeIcon icon={faRightToBracket}/>Logout</Button></Link>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+        ) : (
+          <Container fluid>
+          <Navbar.Brand style={NavbarStyle}><Link to="/" style={myBLink}>Me<span className='text-danger'>llow</span></Link></Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto mx-auto my-2 my-lg-0" style={myLink} navbarScroll>
+              <Link to="/" style={myLink} className="mx-2">Home</Link>
+              <Link to="/recreation" style={myLink} className="mx-2">Recreation</Link> 
+              <Link to="/parks" style={myLink} className="mx-2">Parks</Link>
+              <Link to="/engage" style={myLink} className="mx-2">Engage</Link>
+              <Link to="/dashboard" style={myLink} className="mx-2">Dashboard</Link>
+              <Link to="/dog" style={myLink} className="mx-2">Dogs</Link>
+              <Link to="/about" style={myLink} className="mx-2">About Us</Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+              <Link to="/search" style={myLink}><Button className='shadow-none mx-2' variant="outline-dark">Search</Button></Link>
             </Form>
             <Form className="d-flex">
             <Link to="/signup" style={myLink}><Button className='shadow-none mx-2' variant="outline-dark"><FontAwesomeIcon icon={faUserPlus}/>Sign Up</Button></Link>
@@ -37,6 +82,7 @@ function Navigation() {
             </Form>
           </Navbar.Collapse>
         </Container>
+        ) }
       </Navbar>
     </>
   )
