4c816ef25b29df11c703c7529db3ff08ce8a7b88
Alafonye K. Emmanuel <ke.alafonye@yahoo.com>
2022-04-30 22:03:07 -0600

    User Auth

--- Frontend/mellowgroup/src/Components/PrivateRoute/authentication.js
+++ Frontend/mellowgroup/src/Components/PrivateRoute/authentication.js
@@ -1,12 +1,23 @@
-import React from 'react'
+export const getUser = () => {
 
-const authentication = () => {
-  return (
-    <div>
-     This is git issue testing
-        <Component />
-    </div>
-  )
+    const userStr = sessionStorage.getItem("user")
+    if(userStr) {
+        return JSON.parse(userStr);
+    } else {
+        return null;
+    }
 }
 
-export default authentication
+export const getToken = () => {
+    return sessionStorage.getItem("token") || null;
+}
+
+export const setUserSession = (token, user) => {
+    sessionStorage.setItem("token", token);
+    sessionStorage.setItem("user", JSON.stringify(user));
+}
+
+export const removeUserSession = () => {
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("user");
+}
